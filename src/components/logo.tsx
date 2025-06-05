175e5ffc341eb29d26ddba89f2b32205d1f7d139
Liantsoa <liantsoarandrianasimbolarivelo@gmail.com>
2025-06-05 11:34:37 +0300

    improving logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Logo: React.FC = () => {
     return (
-        <div className="mb-16">
-            <img src="/logo.png" alt="logo" />
+        <div className="mb-16 flex flex-col items-center">
+            <img src="/logo.png" alt="logo"  />
+            <h1 className="text-3xl font-bold text-gray-800">PotatoGuard</h1>
         </div>
     );
 };
